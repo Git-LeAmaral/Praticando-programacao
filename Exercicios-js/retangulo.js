11ac08a4344f207b26bda90566792927819c3473
Leandro Amaral <lramaral02m@gmail.com>
2022-11-05 15:21:37 -0300

    Adicionando Calendario NWL e form dark/light

---
+++ Exercicios-js/retangulo.js
@@ -0,0 +1,20 @@
+// 1. Dado o tamanho da base e da altura de um retângulo, calcular a sua área e o seu perímetro.
+
+function areaRetangulo (a = 1, b = 1){
+    return a * b;
+}
+console.log('A area do retangulo é ' + areaRetangulo(2, 5));
+
+// 2. Dado o tamanho do lado de um quadrado, calcular a área e o perímetro do mesmo.
+
+function areaQuadrado (a, b){
+    return a * b;
+}
+console.log('A área do quadrado é ' + areaQuadrado(4, 4));
+
+// 3. Dado o tamanho do raio de uma circunferência, calcular a área e o perímetro da mesma.
+
+if (condition) {
+    okook
+            }
+
